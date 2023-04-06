777799cefb71128353676b0d183d88fd75386bcf
CarloColletti~ <carlocolletti5@gmail.com>
2023-04-06 18:42:47 +0200

    fill() fix

--- app/Http/Controllers/songController.php
+++ app/Http/Controllers/songController.php
@@ -46,23 +46,23 @@ class songController extends Controller
     {
         $data = $request->all();
 // 1- procedura per salvare tutti i dati singolarmente 
-        $song = new song;
-        $song ->title = $data["title"];
-        $song ->album = $data["album"];
-        $song ->author = $data["author"];
-        $song ->editor = $data["editor"];
-        $song->length = '3.15';
-        $song->poster = 'https://picsum.photos/200/200';
+        // $song = new song;
+        // $song ->title = $data["title"];
+        // $song ->album = $data["album"];
+        // $song ->author = $data["author"];
+        // $song ->editor = $data["editor"];
+        // $song->length = '3.15';
+        // $song->poster = 'https://picsum.photos/200/200';
 
-        $song->save();
+        // $song->save();
 
 // 2- se i data corrispondono con il tipo richiesto si usa un comando solo 
         // da sistemare 
-        // $song = new song;
-        // $song->fill($data);
-        // $song->length = '3.15';
-        // $song->poster = 'https://picsum.photos/200/200';
-        // $song->save();
+        $song = new song;
+        $song->fill($data);
+        $song->length = '3.15';
+        $song->poster = 'https://picsum.photos/200/200';
+        $song->save();
 
         return redirect ()->route('songs.show', $song);
     }
--- app/Models/song.php
+++ app/Models/song.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class song extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'album',
+        'author',
+        'editor',
+    ];
 }
